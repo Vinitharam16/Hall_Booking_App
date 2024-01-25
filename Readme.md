f4148b247cb2c6e891af36ccdcb23f63ae0ae7c3
Vinitharam16 <vinithabegp@gmail.com>
2024-01-26 01:16:20 +0530

    Final Commit

---
+++ Readme.md
@@ -0,0 +1,7 @@
+ postman documentation- https://documenter.getpostman.com/view/31850794/2s9YypFP27
+ 1.view all Rooms and its details-
+ 2.Creating room-
+ 3.Booking room-
+ 4.viewing all booked room data-
+ 5.customers list with booked data-
+ 6.list how many times a customer has booked the room -
--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const HTTP_SERVER = express();
 HTTP_SERVER.use(bodyParser.json());
 
-// postman documentation- https://documenter.getpostman.com/view/31850794/2s9YypDi9M
+// postman documentation- https://documenter.getpostman.com/view/31850794/2s9YypFP27
 
 
 // creating local variables for storing data
@@ -18,8 +18,8 @@ let room = [{
 }];
 
 let bookings = [{
-    customerName: "Ram",
-    bookingDate: "20240101",
+    customer: "Ram",
+    bookingDate: "20240201",
     startTime: "12:00pm",
     endTime: "11.59am",
     bookingID: "A1",
@@ -34,7 +34,7 @@ let customers = [
         bookings: [
             {
                 customer: "Ram",
-                bookingDate: "20240101",
+                bookingDate: "20240201",
                 startTime: "12:00pm",
                 endTime: "11.59am",
                 bookingID: "A1",
@@ -56,123 +56,124 @@ HTTP_SERVER.listen(PORT, () => {
 // view all Rooms and its details
 HTTP_SERVER.get("/room/all", (req, res) => {
     res.status(200).json({
-        RoomsList : room
+        RoomsList: room
     })
-    console.log(room);
 });
 
 // Creating room
-HTTP_SERVER.post('/rooms/create', (req,res) => {
+HTTP_SERVER.post('/rooms/create', (req, res) => {
     const createRoom = req.body;
     const existroomid = room.find((data) => data.roomID === room.roomID)
-    if(existroomid !== undefined){
-        return res.status(200).json({message:"room already exists"});
+    if (existroomid !== undefined) {
+        return res.status(200).json({ message: "room already exists" });
     }
-    else{
+    else {
         room.push(createRoom);
-        res.status(201).json({message:"room created"});
+        res.status(201).json({ message: "room created" });
     }
 
 });
 
 // Booking room
-HTTP_SERVER.post("/booking/create/:id", (req,res) => {
+HTTP_SERVER.post("/booking/create/:id", (req, res) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
         let bookroom = req.body;
         let date = new Date();
         let dateFormat = date.toLocaleDateString();
         let existroomid = room.find((el) => el.roomID === id)
-        console.log(id);
-        if(existroomid !== undefined){
+        if (existroomid !== undefined) {
             return res.status(400).json({
                 message: "room does not exist",
                 RoomsList: room,
             });
-            
+
         }
-    
+
         //verifying booking date
 
         let matchID = bookings.filter((b) => b.roomID === id)
-        if(matchID.length>0){
+        if (matchID.length > 0) {
             let checkDate = matchID.filter((d) => {
                 return d.bookingDate === bookroom.bookingDate
             })
-            if(checkDate.length === 0){
-                let newID = "A"+(bookings.length+1);
-                let newbooking = {...bookroom, bookingID:newID, roomID:id, status:"booked", booked_On:dateFormat}
+            if (checkDate.length === 0) {
+                let newID = "A" + (bookings.length + 1);
+                let newbooking = { ...bookroom, bookingID: newID, roomID: id, status: "booked", booked_On: dateFormat }
                 bookings.push(newbooking);
                 return res.status(201).json({
-                    message:"hall booked",
+                    message: "hall booked",
                     Bookings: bookings,
-                    added:newbooking
+                    added: newbooking
                 });
             }
-            else{
+            else {
                 return res.status(400).json({
-                    message:"hall already booked for this date, choose another hall",
+                    message: "hall already booked for this date, choose another hall",
                     Bookings: bookings
                 });
             }
         }
-        else{
-            let newID = "A"+(bookings.length+1);
-            let newbooking = {...bookroom, bookingID:newID, roomID:id, status:"booked", booked_On:dateFormat}
+        else {
+            let newID = "A" + (bookings.length + 1);
+            let newbooking = { ...bookroom, bookingID: newID, roomID: id, status: "booked", booked_On: dateFormat }
             bookings.push(newbooking);
-            const customerdetails = customers.find((c)=> c.name === newbooking.customer);
-            if(customerdetails){
+            const customerdetails = customers.find((c) => c.name === newbooking.customer);
+            if (customerdetails) {
                 customerdetails.bookings.push(newbooking);
-            } 
-            else{
-                customers.push({ name:newbooking.customer, bookings:[newbooking]});
+            }
+            else {
+                customers.push({ name: newbooking.customer, bookings: [newbooking] });
             }
             return res.status(201).json({
-                message:"hall booked",
-                Bookings:bookings,
-                added:newbooking
-            })
+                message: "hall booked",
+                Bookings: bookings,
+                added: newbooking
+            });
         }
-       
+
     } catch (error) {
         res.status(400).json({
             message: "Error in booking room",
             error: error,
             data: bookings
         });
-        
-    }
+
+    }    
+
+
 })
 
 // viewing all booked room data
 
-HTTP_SERVER.get('/viewbooking',(req,res) => {
+HTTP_SERVER.get('/viewbooking', (req, res) => {
     const bookedrooms = bookings.map(booking => {
-        const {roomID,status,customer,bookingDate,startTime,endTime} =booking;
-        return {roomID,status,customer,bookingDate,startTime,endTime}
+        const { roomID, status, customer, bookingDate, startTime, endTime } = booking;
+        return { roomID, status, customer, bookingDate, startTime, endTime }
     });
     res.status(201).json(bookedrooms);
 })
 
 //customers list with booked data
 
-HTTP_SERVER.get('/customers',(req,res) => {
+HTTP_SERVER.get('/customers', (req, res) => {
     const cutomerBookings = customers.map(customer => {
         const { name, bookings } = customer;
         const customerDetails = bookings.map(booking => {
-            const {roomID, bookingDate, startTime, endTime} = booking;
-            return {name, roomID, bookingDate, startTime, endTime};
+            const { roomID, bookingDate, startTime, endTime } = booking;
+            return { name, roomID, bookingDate, startTime, endTime };
         });
         return customerDetails;
     })
     res.status(201).json(cutomerBookings);
+
 });
 
 // list how many times a customer has booked the room 
-HTTP_SERVER.get('/customer/:name',(req,res) => {
+HTTP_SERVER.get('/customer/:name', (req, res) => {
     const { name } = req.params;
     const customer = customers.find(c => c.name === name);
-    if(!customer) {
+    if (!customer) {
         res.status(400).json({
             error: "Customer not found"
         })
@@ -180,7 +181,7 @@ HTTP_SERVER.get('/customer/:name',(req,res) => {
     }
     const customerbookings = customer.bookings.map(booking => {
         const { customer, roomID, bookingDate, startTime, endTime, bookingID, booked_On, status } = booking;
-        return {customer, roomID, bookingDate, startTime, endTime, bookingID, booked_On, status};
+        return { customer, roomID, bookingDate, startTime, endTime, bookingID, booked_On, status };
     });
     res.status(201).json(customerbookings);
 })
